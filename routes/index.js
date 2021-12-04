e577a73fad79c2ecc0a622de0b97842a113b974f
fober <fober@gmx.de>
2021-12-04 16:00:17 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express powered by Plesk' });
+  res.render('index', { title: 'Express powered by Plesk hosted by fober' });
 });
 
 module.exports = router;
